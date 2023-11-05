a18230103f6a1f65393216923f77ec373dfad107
Tanh2003 <nhoxtuananh092@gmail.com>
2023-11-05 15:11:01 +0700

    code video 76

---
+++ Nhom4_Healthy_Care-main/Nhom4_Healthy_Care-main/NodeJs/src/controllers/doctorController.js
@@ -0,0 +1,89 @@
+import doctorServices from "../services/doctorServices";
+
+let getTopDoctorHome = async (req, res) => {
+  let limit = req.query.limit;
+
+  if (!limit) limit = 10;
+  try {
+    let response = await doctorServices.getTopDoctorHome(+limit);
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errcode: -1,
+      message: "Error from sever....",
+    });
+  }
+};
+let getAllDoctors = async (req, res) => {
+  try {
+    let doctors = await doctorServices.getAllDoctors();
+    return res.status(200).json(doctors);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errcode: -1,
+      errMessage: "Error from sever...",
+    });
+  }
+};
+let postInfoDoctor = async (req, res) => {
+  try {
+    let response = await doctorServices.saveDetailInforDoctor(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errcode: -1,
+      errMessage: "Error from sever...",
+    });
+  }
+};
+
+let getDetailDoctorById = async (req, res) => {
+  try {
+    let infor = await doctorServices.getDetailDoctorById(req.query.id);
+    return res.status(200).json(infor);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errcode: -1,
+      errMessage: "Error from sever .. . . .",
+    });
+  }
+};
+let bulkCreateSchedule = async (req, res) => {
+  try {
+    let infor = await doctorServices.bulkCreateSchedule(req.query);
+    return res.status(200).json(infor);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errcode: -1,
+      errMessage: "Error from sever .. . . .",
+    });
+  }
+};
+let getScheduleByDate = async (req, res) => {
+  try {
+    let infor = await doctorServices.getScheduleByDate(
+      req.query.doctorId,
+      req.query.date
+    );
+    return res.status(200).json(infor);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errcode: -1,
+      errMessage: "Error from sever .. . . .",
+    });
+  }
+};
+module.exports = {
+  getTopDoctorHome: getTopDoctorHome,
+  getAllDoctors: getAllDoctors,
+  postInfoDoctor: postInfoDoctor,
+  getDetailDoctorById: getDetailDoctorById,
+  bulkCreateSchedule: bulkCreateSchedule,
+  getScheduleByDate: getScheduleByDate,
+};
---
+++ Nhom4_Healthy_Care-main/Nhom4_Healthy_Care-main/NodeJs/src/services/doctorServices.js
@@ -0,0 +1,239 @@
+import db from "../models/index";
+require("dotenv").config();
+import _ from "lodash";
+const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
+let getTopDoctorHome = (limitInput) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let users = await db.User.findAll({
+        limit: limitInput,
+        where: { roleId: "R2" },
+        order: [["createdAt", "DESC"]],
+        attributes: {
+          exclude: ["password"],
+        },
+        include: [
+          {
+            model: db.Allcode,
+            as: "positionData",
+            attributes: ["valueEn", "valueVi"],
+          },
+          {
+            model: db.Allcode,
+            as: "genderData",
+            attributes: ["valueEn", "valueVi"],
+          },
+        ],
+        raw: true,
+        nest: true,
+      });
+
+      resolve({
+        errcode: 0,
+        data: users,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+let getAllDoctors = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let doctors = await db.User.findAll({
+        where: { roleId: "R2" },
+        attributes: {
+          exclude: ["password", "image"],
+        },
+      });
+
+      resolve({
+        errcode: 0,
+        data: doctors,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+let saveDetailInforDoctor = (InputData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (
+        !InputData.doctorId ||
+        !InputData.contentHTML ||
+        !InputData.contentMarkdown ||
+        !InputData.action
+      ) {
+        resolve({
+          errcode: 1,
+          errMessage: "missing parameter",
+          InputData,
+        });
+      } else {
+        if (InputData.action === "CREATE") {
+          await db.Markdown.create({
+            contentHTML: InputData.contentHTML,
+            contentMarkdown: InputData.contentMarkdown,
+            description: InputData.description,
+            doctorId: InputData.doctorId,
+          });
+        } else if (InputData.action === "EDIT") {
+          let doctorMarkdown = await db.Markdown.findOne({
+            where: { doctorId: InputData.doctorId },
+            raw: false,
+          });
+          if (doctorMarkdown) {
+            doctorMarkdown.contentHTML = InputData.contentHTML;
+            doctorMarkdown.contentMarkdown = InputData.contentMarkdown;
+            doctorMarkdown.description = InputData.description;
+
+            await doctorMarkdown.save();
+          }
+        }
+
+        resolve({
+          errcode: 0,
+          errMessage: "Save infor doctor succeed!",
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+let getDetailDoctorById = (inputId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!inputId) {
+        resolve({
+          errcode: 1,
+          errMessage: "Missing required parameter!",
+        });
+      } else {
+        let data = await db.User.findOne({
+          where: {
+            id: inputId,
+          },
+          attributes: {
+            exclude: ["password"],
+          },
+          include: [
+            {
+              model: db.Markdown,
+              attributes: ["description", "contentHTML", "contentMarkdown"],
+            },
+            {
+              model: db.Allcode,
+              as: "positionData",
+              attributes: ["valueEn", "valueVi"],
+            },
+          ],
+          raw: false,
+          nest: true,
+        });
+        // code nayf convert anh de sai
+
+        if (data && data.image) {
+          data.image = Buffer.from(data.image, "base64").toString("binary");
+        }
+        if (!data) {
+          data = {};
+        }
+        resolve({
+          errcode: 0,
+          data: data,
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+let bulkCreateSchedule = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.arrSchedule || !data.doctorId || !data.formatedDate) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing required param!",
+        });
+      } else {
+        let schedule = data.arrSchedule;
+        if (schedule && schedule.length > 0) {
+          schedule = schedule.map((item) => {
+            item.maxNumber = MAX_NUMBER_SCHEDULE;
+            return item;
+          });
+        }
+        let existing = await db.Schedule.findAll({
+          where: { doctorId: data.doctorId, date: data.formatedDateP },
+          attributes: ["timeType", "date", "doctorId", "maxNumber"],
+          raw: true,
+        });
+        // if (existing && existing.length > 0) {
+        //   existing = existing.map((item) => {
+        //     item.date = new Date(item.date).getTime();
+        //     return item;
+        //   });
+        // }
+        let toCreate = _.differenceWith(schedule, existing, (a, b) => {
+          // return a.timeType === b.timeType && a.date === b.date;
+          return a.timeType === b.timeType && +a.date === +b.date;
+        });
+        if (toCreate && toCreate.length > 0) {
+          await db.Schedule.bulkCreate(toCreate);
+        }
+        resolve({
+          errCode: 1,
+          errMessage: "OK",
+        });
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+let getScheduleByDate = (doctorId, date) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!doctorId || !date) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing required parameters",
+        });
+      } else {
+        let dataSchedule = await db.Schedule.findAll({
+          where: {
+            doctorId: doctorId,
+            date: date,
+          },
+          include: [
+            {
+              model: db.Allcode,
+              as: "timeTypeData",
+              attributes: ["valueEn", "valueVi"],
+            },
+          ],
+          raw: false,
+          nest: true,
+        });
+        if (!dataSchedule) dataSchedule = [];
+        resolve({
+          errCode: 0,
+          data: dataSchedule,
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+module.exports = {
+  getTopDoctorHome: getTopDoctorHome,
+  getAllDoctors: getAllDoctors,
+  saveDetailInforDoctor: saveDetailInforDoctor,
+  getDetailDoctorById: getDetailDoctorById,
+  bulkCreateSchedule: bulkCreateSchedule,
+  getScheduleByDate: getScheduleByDate,
+};
